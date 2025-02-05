976bfb34a351237085af682ee67228f2ebf1bdbf
John <jgfriedman99@gmail.com>
2025-02-04 18:07:10 -0800

    python imports fix

--- doc2dict/doc2dict/__init__.py
+++ doc2dict/doc2dict/__init__.py
@@ -2,3 +2,5 @@ from .html.html_parser import html_reduction
 from .html.visualizer import format_list_html
 from .xml.parser import xml2dict
 from .txt.parser import txt2dict
+
+__all__ = ['html_reduction', 'format_list_html', 'xml2dict', 'txt2dict']
