ad62cf401dab496aef8fb6cb10bf47586c9d877d
John Friedman <jgfriedman99@gmail.com>
2025-04-16 19:56:25 -0700

    dump

--- doc2dict/doc2dict/xml/parser.py
+++ doc2dict/doc2dict/xml/parser.py
@@ -1,7 +1,11 @@
 import xmltodict
 from ..mapping import JSONTransformer
 
-def remove_namespace(path, key, value):
+def remove_namespace_and_none(path, key, value):
+    # Skip this key-value pair if value is None
+    if value is None:
+        return None  # Return None to exclude this key-value pair
+    
     # Remove namespace from keys
     if ':' in key:
         # Keep only the part after the last colon
@@ -9,13 +13,16 @@ def remove_namespace(path, key, value):
     return key, value
 
 def xml2dict(content, mapping_dict=None):
-
-    data = xmltodict.parse(content,postprocessor=remove_namespace)
+    data = xmltodict.parse(
+        content,
+        postprocessor=remove_namespace_and_none,
+        process_namespaces=True,  # Handle namespaces
+        namespaces={}
+    )
 
     if mapping_dict is None:
         return data
     
-     
     transformer = JSONTransformer(mapping_dict)
     transformed_data = transformer.transform(data)
     return transformed_data
