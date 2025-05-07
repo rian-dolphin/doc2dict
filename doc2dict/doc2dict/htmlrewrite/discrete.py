90787f2523948fee83ceaf85c7d7060645fbe6ff
John Friedman <jgfriedman99@gmail.com>
2025-05-06 22:17:06 -0700

    working on discrete

--- doc2dict/doc2dict/htmlrewrite/discrete.py
+++ doc2dict/doc2dict/htmlrewrite/discrete.py
@@ -5,13 +5,31 @@
 def convert_instructions_to_discrete(instructions):
     current_line = []
     lines = []
-    attributes = {}
+
+    attributes = {'bold':0, 'italic':0, 'underline':0, 'all_caps':0,'text:center':0,'font-size':[]}
+
+    bool_attributes = ['bold', 'italic', 'underline', 'all_caps', 'text:center']
+    
     for instruction in instructions:
         if not instruction:
             lines.append(current_line)
             current_line = []
         
         if 'text' in instruction:
-            current_line.append(attributes | {'text': instruction['text']})
+            current_dict_attributes = {}
+            for bool_attribute in bool_attributes:
+                if attributes[bool_attribute] > 0:
+                    current_dict_attributes[bool_attribute] = True
+            current_line.append(current_dict_attributes| {'text': instruction['text']})
+
+        if 'start' in instruction:
+            for bool_attribute in bool_attributes:
+                if bool_attribute in instruction['start']:
+                    attributes[bool_attribute] += 1
+
+        elif 'end' in instruction:
+            for bool_attribute in bool_attributes:
+                if bool_attribute in instruction['end']:
+                    attributes[bool_attribute] -= 1
 
     return lines
--- test.py
+++ test.py
@@ -1,23 +1,29 @@
 from selectolax.parser import HTMLParser
 from doc2dict.doc2dict.htmlrewrite.flow import convert_html_to_flow
 from doc2dict.doc2dict.htmlrewrite.discrete import convert_instructions_to_discrete
-            
-        
+from time import time
 
 # Benchmark bottom 150ms
 
+start = time()
 file_path=r'C:\Users\jgfri\OneDrive\Desktop\test\test\0000320193-24-000123\aapl-20240928.htm'
 with open(file_path, 'r', encoding='utf-8') as f:
     parser = HTMLParser(f.read())
 
+print("load time:", time()-start)
+
+start = time()
 body = parser.body
 instructions = convert_html_to_flow(body)
+print("convert to instructions time:", time()-start)
 
 with open('instructions.txt', 'w', encoding='utf-8') as f:
     for instruction in instructions:
         f.write(str(instruction) + '\n')
 
+start = time()
 lines = convert_instructions_to_discrete(instructions)
+print("convert to discrete time:", time()-start)
 with open('discrete.txt', 'w', encoding='utf-8') as f:
     for line in lines:
         f.write(str(line) + '\n')
