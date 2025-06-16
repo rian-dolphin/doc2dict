99feab8011264f88222ce7950b4b84d6a10e81d8
John Friedman <jgfriedman99@gmail.com>
2025-06-16 12:16:31 -0700

    wow! selectolax updated fast

--- doc2dict/setup.py
+++ doc2dict/setup.py
@@ -2,13 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="doc2dict",
-    version="0.3.5",
+    version="0.3.6",
     packages=find_packages(),
-    install_requires=[
-        'Cython==3.0.11',  # Pin the exact version selectolax needs
-        'selectolax',
-        'xmltodict',
-        'pypdfium2'
+    install_requires=['selectolax','xmltodict','pypdfium2'
     ],
     python_requires=">=3.8"
 )
